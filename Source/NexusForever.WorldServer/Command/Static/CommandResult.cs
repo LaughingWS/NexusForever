0a4bc3a976ab408f9e8d9f723b141abd5af75781
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-10-02 06:53:33 -0400

    10/2/24

---
+++ Source/NexusForever.WorldServer/Command/Static/CommandResult.cs
@@ -0,0 +1,12 @@
+namespace NexusForever.WorldServer.Command.Static
+{
+    public enum CommandResult
+    {
+        Ok,
+        NoPermission,
+        NoCommand,
+        InvalidParameters,
+        InvalidContext,
+        InvalidTarget
+    }
+}
