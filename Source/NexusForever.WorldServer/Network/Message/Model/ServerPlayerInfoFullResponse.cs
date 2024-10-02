9d3333af48525481b41bb84a797617d6b668b538
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-10-02 18:03:49 -0400

    10/2/24

---
+++ Source/NexusForever.WorldServer/Network/Message/Model/ServerPlayerInfoFullResponse.cs
@@ -0,0 +1,32 @@
+using NexusForever.Shared.Network;
+using NexusForever.Shared.Network.Message;
+using NexusForever.WorldServer.Game.Entity.Static;
+using NexusForever.WorldServer.Game.Reputation.Static;
+using NexusForever.WorldServer.Network.Message.Model.Shared;
+
+namespace NexusForever.WorldServer.Network.Message.Model
+{
+    [Message(GameMessageOpcode.ServerPlayerInfoFullResponse)]
+    public class ServerPlayerInfoFullResponse : IWritable
+    {
+        public ServerPlayerInfoBasicResponse BaseData { get; set; }
+
+        public bool IsClassPathSet { get; set; } = true;
+        public Path Path { get; set; }
+        public Class Class { get; set; }
+        public uint Level { get; set; }
+        public bool IsLastLoggedOnInDaysSet { get; set; }
+        public float LastLoggedInDays { get; set; }
+
+        public void Write(GamePacketWriter writer)
+        {
+            BaseData.Write(writer);
+            writer.Write(IsClassPathSet);
+            writer.Write(Path, 3u);
+            writer.Write(Class, 14u);
+            writer.Write(Level);
+            writer.Write(IsLastLoggedOnInDaysSet);
+            writer.Write(LastLoggedInDays);
+        }
+    }
+}
---
+++ Source/NexusForever.WorldServer/Network/Message/Model/ServerVehiclePassengerSet.cs
@@ -0,0 +1,29 @@
+﻿using NexusForever.Shared.Network;
+using NexusForever.Shared.Network.Message;
+using NexusForever.WorldServer.Game.Entity.Static;
+
+namespace NexusForever.WorldServer.Network.Message.Model
+{
+    /// <summary>
+    /// Should only be used during <see cref="GridEntity"/> AddVisible() method. It is used to "connect" an entity to their vehicle.
+    /// </summary>
+    /// <remarks>
+    /// Packets indicate this was only used when an entity and their vehicle were created at the same time, e.g. Player logs in and another Player nearby is already mounted.
+    /// </remarks>
+    [Message(GameMessageOpcode.ServerVehiclePassengerSet)]
+    public class ServerVehiclePassengerSet : IWritable
+    {
+        public uint Self { get; set; }
+        public uint Vehicle { get; set; }
+        public VehicleSeatType SeatType { get; set; }
+        public byte SeatPosition { get; set; }
+
+        public void Write(GamePacketWriter writer)
+        {
+            writer.Write(Self);
+            writer.Write(Vehicle);
+            writer.Write(SeatType, 2u);
+            writer.Write(SeatPosition, 3u);
+        }
+    }
+}
