bcc40872b390211f9101c8c56e13053343e21688
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-10-02 18:09:30 -0400

    10/2/24

---
+++ Source/NexusForever.WorldServer/Network/Message/Model/Server08B3.cs
@@ -0,0 +1,20 @@
+﻿using NexusForever.Shared.Network;
+using NexusForever.Shared.Network.Message;
+
+namespace NexusForever.WorldServer.Network.Message.Model
+{
+    [Message(GameMessageOpcode.Server08B3)]
+    public class Server08B3 : IWritable
+    {
+        public uint UnitId { get; set; }
+        public uint Unknown0 { get; set; }
+        public bool Unknown1 { get; set; }
+
+        public void Write(GamePacketWriter writer)
+        {
+            writer.Write(UnitId);
+            writer.Write(Unknown0);
+            writer.Write(Unknown1);
+        }
+    }
+}
