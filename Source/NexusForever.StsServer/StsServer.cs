c054ac05bcd8cee19412055bb5a1c0d7d7eb5d71
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-10-02 17:33:32 -0400

    10/2/24

---
+++ Source/NexusForever.StsServer/StsServer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Systemd;
+using Microsoft.Extensions.Hosting.WindowsServices;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+
+namespace NexusForever.StsServer
+{
+    internal static class StsServer
+    {
+        #if DEBUG
+        private const string Title = "NexusForever: STS Server (DEBUG)";
+        #else
+        private const string Title = "NexusForever: STS Server (RELEASE)";
+        #endif
+
+        private static readonly NLog.ILogger log = LogManager.GetCurrentClassLogger();
+
+        private static void Main()
+        {
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+
+            IHostBuilder builder = new HostBuilder()
+                .ConfigureLogging(lb =>
+                {
+                    // only applicable to logging done through host
+                    // other logging is still done directly though NLog
+                    lb.ClearProviders()
+                        .SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace)
+                        .AddNLog();
+                })
+                .ConfigureServices(sc =>
+                {
+                    sc.AddHostedService<HostedService>();
+                })
+                .UseWindowsService()
+                .UseSystemd();
+
+            if (!WindowsServiceHelpers.IsWindowsService() && !SystemdHelpers.IsSystemdService())
+                Console.Title = Title;
+
+            try
+            {
+                IHost host = builder.Build();
+                host.Run();
+            }
+            catch (Exception e)
+            {
+                log.Fatal(e);
+            }
+        }
+    }
+}
