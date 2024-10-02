fa864144cb87ac91fe33f652f64547f82834ab80
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-10-02 17:37:02 -0400

    10/2/24

---
+++ Source/NexusForever.Shared/Configuration/SharedConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace NexusForever.Shared.Configuration
+{
+    public static class SharedConfiguration
+    {
+        public static IConfiguration Configuration { get; private set; }
+
+        public static void Initialise(string file)
+        {
+            if (Configuration != null)
+                return;
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile(file, false, true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(Environment.GetCommandLineArgs().Skip(1).ToArray());
+
+            Configuration = builder.Build();
+        }
+    }
+}
