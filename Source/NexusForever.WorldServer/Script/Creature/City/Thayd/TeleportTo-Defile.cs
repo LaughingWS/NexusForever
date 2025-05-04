863b6f2d9d5d773a8cf0327ea67e9d3336c22782
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2025-05-03 23:09:42 -0400

    Thayd to Defile portal

---
+++ Source/NexusForever.WorldServer/Script/Creature/City/Thayd/TeleportTo-Defile.cs
@@ -0,0 +1,37 @@
+﻿using NexusForever.Shared;
+using NexusForever.Shared.GameTable;
+using NexusForever.Shared.GameTable.Model;
+using NexusForever.WorldServer.Game.Entity;
+using NexusForever.WorldServer.Game.Map;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexusForever.WorldServer.Script.Creature.City.Thayd
+{
+    [Script(70385)]
+    public class TeleportTo_Defile : CreatureScript
+    {
+        const uint WLOC_HopesDare = 49900;
+
+        public override void OnActivateSuccess(WorldEntity me, WorldEntity activator)
+        {
+            if (activator is not Player player)
+                return;
+
+            WorldLocation2Entry entry = GameTableManager.Instance.WorldLocation2.GetEntry(WLOC_HopesDare);
+            if (entry == null)
+                return;
+
+            if (!player.CanTeleport())
+                return;
+
+            var rotation = new Quaternion(entry.Facing0, entry.Facing1, entry.Facing2, entry.Facing3);
+            player.Rotation = rotation.ToEulerDegrees();
+            player.TeleportTo((ushort)entry.WorldId, entry.Position0, entry.Position1, entry.Position2);
+        }
+    }
+}
